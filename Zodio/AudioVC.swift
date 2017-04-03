b752dc4852c7b827b75ad5a28b4823e15aa81648
Roydon Jeffrey <brushdam14823@gmail.com>
2017-04-03 17:04:52 -0400

    Added the necessary files for the structure in which I am thinking about building my app.

---
+++ Zodio/AudioVC.swift
@@ -0,0 +1,11 @@
+
+import UIKit
+
+class AudioVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
---
+++ Zodio/MainVC.swift
@@ -0,0 +1,11 @@
+
+import UIKit
+
+class MainVC: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
